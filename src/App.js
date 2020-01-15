6017990116ffe95266500d9c4d884f8147909d92
SlickJacket <novenarii@gmail.com>
2020-01-15 01:38:13 -0500

    complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state= {
+    events: [],
+    arrayOfLocations: [],
+    toggle: false,
+    filteredEvents: []
+  }
+
+  // Fetching data from github page and saving the array of events in state
+  componentDidMount() {
+    fetch('https://gist.githubusercontent.com/SlickJacket/3165d3e2605e4f4f29557afaf30a1cc7/raw/ca7ee6f07a4d0ef1cf9b3bbf32b8cc058bc675fe/eventlist.js') 
+    .then(res => res.json())
+    .then(data => this.setState({events: data.Items}))
+
+}
+
+// Depending on if the toggle has been flipped to true or false, the renderEvents function will render out the User's 
+// Filtered events
+  renderEvents = () => {
+    if (this.state.toggle === true) {
+      return this.state.filteredEvents.map(function(item) {
+        return <div id="card">
+                      <h2 style={{color: "#0e1111"}}>{item.VenueName}</h2>
+                      <h3 style={{color: "white"}}>{item.MinPrice}</h3>
+                      <p>{item.Date}, {item.Time}</p>
+                  </div>
+    })
+    } else {
+      return this.state.events.map(function(item) {
+        return <div id="card">
+                      <h2 style={{color: "#0e1111"}}>{item.VenueName}</h2>
+                      <h3 style={{color: "white"}}>{item.MinPrice}</h3>
+                      <p>{item.Date}, {item.Time}</p>
+                  </div>
+    })
+    }
+    
+      
+  }
+
+  // Helper method. This method's purpose is simply to add all of the venue cities to the arrayOfLocations state. 
+  locationOptions = () => {
+    const arrayOfLocations =this.state.arrayOfLocations
+    return this.state.events.map(function(item) {
+      arrayOfLocations.push(item.VenueCity);
+    })
+  }
+
+// Calling the locationOptions method to populate the arrayOfLocations Array
+// Then, converting that to a Set in order to filter out the duplicates
+// Finally, converting the Set back into an Array to be iterated through for displaying 
+// the locations in the dropdown menu
+  renderLocations = () => {
+    this.locationOptions()
+    const uniqueSet = new Set(this.state.arrayOfLocations)
+    const backToArray = [...uniqueSet]
+        return backToArray.map(function(loc) {
+            return <option>{loc}</option>
+    })
+  }
+
+// Handling the User's click in the dropdown menu. 
+// The first conditional is simply checking if the User has selected "All" or not.
+// Depending on their choice, then the toggle state will be changed accordingly
+// Finally, adding the events of the location the User has chosen to the filteredEvents state to
+// be used in the renderEvents method
+  handleClick = (e) => {
+    e.preventDefault();
+    const filteredEvents = this.state.filteredEvents
+    this.state.filteredEvents.length = 0
+    if (e.target.value != "All") {
+      this.setState({toggle: true})
+    }else {
+      this.setState({toggle: false});
+    }
+    return this.state.events.map(function(item) {
+      if (item.VenueCity === e.target.value) {
+        filteredEvents.push(item)
+        
+      } 
+      })
+  }
+
+
+
+  render() { 
+    return ( 
+
+        <div>
+          <h1>Elton John</h1>
+
+            <div class="dropdown">
+              <button class="dropbtn"  style={{fontWeight: "900"}}>Locations</button>
+              <div class="dropdown-content" onClick={this.handleClick}>
+                <option>All</option>
+                {this.renderLocations()}
+              </div>
+            </div>
+
+            <div id="cardFlex">
+              {this.renderEvents()}
+            </div>
+        </div>);
+  }
 }
 
 export default App;
